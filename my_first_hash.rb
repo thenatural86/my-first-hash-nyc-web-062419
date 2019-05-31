b1c051fc85e671b2dbdc69850daea268b036989d
The Natural <philip.smalls86@gmail.com>
2019-05-31 01:05:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,8 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-                 shipping_manifest["oil paintings"]
+                 
+  shipping_manifest["oil paintings"]
 end
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   
