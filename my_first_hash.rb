fb5da454b0837e4cc3dfde8f8b0480fe18634864
The Natural <philip.smalls86@gmail.com>
2019-05-31 00:54:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,12 +24,12 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+end
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   
-shipping_manifest["oil paintings"]
-end
 
+
+shipping_manifest["oil paintings"]
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
